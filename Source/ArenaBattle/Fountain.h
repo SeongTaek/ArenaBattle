3dea7de2bee5125c739bcdf5e1581a12c6f39666
creataek <ljslove119@naver.com>
2020-12-12 22:46:10 +0900

    CHAPTER 1~2

--- Source/ArenaBattle/Fountain.h
+++ Source/ArenaBattle/Fountain.h
@@ -2,16 +2,17 @@
 
 #pragma once
 
-#include "CoreMinimal.h"
+#include "EngineMinimal.h"
 #include "GameFramework/Actor.h"
+#include "ArenaBattle.h"
 #include "Fountain.generated.h"
 
 UCLASS()
 class ARENABATTLE_API AFountain : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
+
+public:
 	// Sets default values for this actor's properties
 	AFountain();
 
@@ -19,8 +20,23 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
+public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+public:
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* Body;
+	 
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* Water;
+
+	UPROPERTY(VisibleAnywhere)
+	UPointLightComponent* Light;
+
+	UPROPERTY(VisibleAnywhere)
+	UParticleSystemComponent* Splash;
+
+	UPROPERTY(EditAnywhere, Category=ID)
+	int32 ID;
 };
